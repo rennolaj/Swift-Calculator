1e4df471913ec32d815e8cf073d67cd8c1db786c
Javier Rennola <jrennola@gmail.com>
2016-08-16 18:29:05 -0400

    First Commit

---
+++ Calculator/Assets.xcassets/CalculatorBrain.swift
@@ -0,0 +1,84 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Javier Rennola on 1/7/16.
+//  Copyright © 2016 Javier Rennola. All rights reserved.
+//
+
+import Foundation
+
+class CalulatorBrain
+{
+    private enum Op
+    {
+     case Operand(Double)
+     case UnaryOperation(String, Double->Double)
+     case BinaryOperation(String , (Double,Double)->Double)
+    }
+    
+    private var opStack = [Op]()
+
+    private var knownOps = [String:Op]()
+    
+    init()
+    {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷", {$1 / $0})
+        knownOps["−"] = Op.BinaryOperation("−", {$1 - $0})
+        knownOps["+"] = Op.BinaryOperation("+", {$0 + $1})
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+        knownOps["sin"] = Op.UnaryOperation("sin", sin)
+        knownOps["cos"] = Op.UnaryOperation("cos", cos)
+        knownOps["tan"] = Op.UnaryOperation("tan", tan)
+        
+    }
+    
+   @nonobjc private func evaluate(ops: [Op] )->(result:Double?,remainingOps:[Op])
+   {
+    if !ops.isEmpty {
+        var remainingOps = ops
+        let op = remainingOps.removeLast()
+        
+        switch op{
+        case .Operand(let operand):
+            return (operand,remainingOps)
+        case .UnaryOperation(_, let operation):
+            let operandEvaluation = evaluate(remainingOps)
+            if let operand = operandEvaluation.result{
+             return (operation (operand),operandEvaluation.remainingOps)
+            }
+        case .BinaryOperation(_,let operation):
+            let op1Evaluation = evaluate(remainingOps)
+            if let operand1 = op1Evaluation.result{
+                let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                if let operand2 = op2Evaluation.result{
+                    return (operation(operand1,operand2),op2Evaluation.remainingOps)
+                }
+            }
+            
+        }
+    }
+    return (nil, ops)
+   }
+   
+    @nonobjc func evaluate() -> Double?
+    {
+      let (result , _) = evaluate(opStack)
+        return result
+    }
+   func pushOperand (operand: Double) -> Double?
+    {
+        opStack.append(Op.Operand(<#T##Double#>))
+        return evaluate()
+    }
+    
+    func performOperation (symbol: String)
+    {
+        if let operation = knownOps[symbol]
+        {
+            opStack.append(operation)
+        }
+    }
+
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,114 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+  
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingNumber: Bool=false
+    
+    var brain = CalulatorBrain()
+    
+    
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber
+        {
+         display.text = display.text! + digit
+         print("digit = \(digit)")
+        }
+        else
+        {
+         display.text = digit
+         userIsInTheMiddleOfTypingNumber = true
+        }
     }
+    
+    
+  @IBAction func operate(sender: UIButton) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//      let operation = sender.currentTitle!
+      if userIsInTheMiddleOfTypingNumber {
+         enter()
+        }
+    if  let operation = sender.currentTitle
+    {
+        if  let result = brain.performOperation(operation)
+        {
+    
+            displayValue = result
+        }
+        else
+        {
+            displayValue = 0
+        }
     }
+//        switch operation
+//        {
+//        case"×":performOperation {$0*$1}
+//        case"÷":performOperation {$1/$0}
+//        case"+":performOperation {$0+$1}
+//        case"−":performOperation {$1-$0}
+//        case"sin":performOperation2 {sin($0)}
+//        case"cos":performOperation2 {cos($0)}
+//        case"tan":performOperation2 {tan($0)}
+//       default: break
+//        }
+        
+        
+    }
+//        func performOperation (operation:(Double,Double)->Double)
+//    {
+//        if operandStack.count>=2
+//        {
+//            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+//            enter()
+//        }
+//    }
+//    func performOperation2 (operation:Double->Double)
+//    {
+//        if operandStack.count>=1
+//        {
+//            displayValue = operation(operandStack.removeLast())
+//            enter()
+//        }
+//    }
+    
+//        var operandStack = Array<Double>()
+    
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+       if let result = brain.pushOperand(displayValue)
+       {
+        displayValue = result
+        }
+        else
+       {
+        displayValue = 0 // display value took optional
+        }
+        
+//        operandStack.append(displayValue)
+//        print("operandStack = \(operandStack)")
 
-
-}
+        
+    }
+    
+    
+    var displayValue : Double
+    {
+        get
+        {
+         return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set
+        {
+         display.text = "\(newValue)"
+         userIsInTheMiddleOfTypingNumber = false
+        }
+    }
+ }
 
